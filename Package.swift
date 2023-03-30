07acaee939a17bc5b0fb6c2cc0f67dec98bccd5e
Sandis Millers <sandis@chi.lv>
2023-03-30 14:42:24 +0300

    Multi-target support

--- Package.swift
+++ Package.swift
@@ -8,11 +8,12 @@ let package = Package(
     products: [
         .library(
             name: "Mobilisten",
-            targets: ["Mobilisten"]),
+            targets: ["Mobilisten", "_MobilistenStub"]),
     ],
     targets: [
         .binaryTarget(
             name: "Mobilisten",
-            url: "https://raw.githubusercontent.com/zoho/SalesIQ-Mobilisten-iOS/v\(version)/Mobilisten.zip", checksum: "10d16a2fcc868314f78976ee7acd16dc285b9c5e8b227a7cbebf75b4d63e6081")
+            url: "https://raw.githubusercontent.com/zoho/SalesIQ-Mobilisten-iOS/v\(version)/Mobilisten.zip", checksum: "10d16a2fcc868314f78976ee7acd16dc285b9c5e8b227a7cbebf75b4d63e6081"),
+        .target(name: "_MobilistenStub"),
     ]
 )
---
+++ Sources/_MobilistenStub/Stub.swift
@@ -0,0 +1,8 @@
+//
+//  Stub.swift
+//  
+//
+//  Created by Sandis Millers on 30/03/2023.
+//
+
+import Mobilisten
